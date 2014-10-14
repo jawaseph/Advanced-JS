c88682389a3284912bda09fbf95f59fc8e4318d5
Joe Calamia <joseph.calamia@yale.edu>
2014-10-14 19:20:40 -0400

    hw

---
+++ classwork/week4/javascripts/angular/controllers/my_app.js
@@ -0,0 +1,15 @@
+angular.module("MyApp", ["ui.router"])
+	.config(function($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state("home", {
+				url: "/",
+				templateUrl: "./templates/home.html"
+			})
+			.state("profile", {
+				url: "/profile",
+				templateUrl: "./templates/profile.html",
+				controller: "ProfileCtrl"
+			});
+			//Make the default route "/" instead of nothing 
+			$urlRouterProvider.otherwise("/");
+	}
